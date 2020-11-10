eb47ba0276cdd806d9baccf118a613441aba0b8c
cep104 <candicelizabethpeters@gmail.com>
2020-11-09 18:30:04 -0600

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,20 +5,21 @@
 # }
 
 def happy_birthday(birthday_kids)
- birthday_kids.each do
-   |name, age|
-   puts "Happy Birthday #{name}! You are now #{age} years old!"
-end
+  birthday_kids.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
+  # add your code snippet here!
 end
 
 def age_appropriate_birthday(birthday_kids)
- birthday_kids.each do
-   |name, age|
-  if age < 13
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  birthday_kids.each do |kid,age|#hash key=kid, value=age
+    if age < 12 #kid is less than 12 put this
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"   
+    else #older than 12 put this
+      puts "You are too old for this."
+    
+    end
   end
-end
-puts "You are too old for this."
 end
 
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,14 +5,25 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
-
 def select_winner(passengers)
   winner = ""
-  passengers.each do
-    |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+ 
 winner
 end
+# def select_winner(passengers)
+#   winner = "" make a variable called winner
+#   passengers.each do |k,v| itterate using key and value
+#     if k == :suite_a && v.start_with?("A")
+  #if key = :suite_a and value starts with A
+#       winner = v
+#     end
+#   end
+#   winner
+#   # add the code snippet here!
+# end
+
